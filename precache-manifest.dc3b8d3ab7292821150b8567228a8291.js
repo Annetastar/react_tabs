80101dac1aae63457a2457c65a93abab64df0f18
Anna Kazantseva <annetastara@gmail.com>
2019-10-29 20:33:06 +0200

    Updates

---
+++ precache-manifest.dc3b8d3ab7292821150b8567228a8291.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "296a0ddfaffa14b3c05c31b51fb97a80",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "26abaeeb61b1f43c093a",
+    "url": "/react_tabs/static/css/main.65938976.chunk.css"
+  },
+  {
+    "revision": "ec14eb012e5d855c520e",
+    "url": "/react_tabs/static/js/2.9fbf4d89.chunk.js"
+  },
+  {
+    "revision": "26abaeeb61b1f43c093a",
+    "url": "/react_tabs/static/js/main.42ee2624.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
